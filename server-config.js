4fb6be65c333d834c6f9ca456f64737093ff5cf4
Michael Roberts <m_roberts6@u.pacific.edu>
2015-03-13 15:32:10 -0700

    (feature) add configuration details to server-config.js

--- server-config.js
+++ server-config.js
@@ -8,6 +8,14 @@ var app = express();
 
 app.configure(function() {
   // write app.set and app.use here
+
+  // app.set('views', __dirname = '/views'); // this assumes all views are located in a folder named views
+  app.set('view engine', 'ejs');
+  app.use(partials());
+  app.use(express.bodyParser());
+  // app.use(express.static(__dirname + '/public')); // this assumes all client files are in a folder named public
+  app.use(express.session());
+
 });
 
   // map routes to request-handler here
