f2eaa94a960a7da4d3f7cea7a106802af3d5fc3e
Michael Roberts <m_roberts6@u.pacific.edu>
2015-03-13 14:14:57 -0700

    (refactor) set up back end files to be skeleton files that run with nodemon successfuly after npm install

--- server-config.js
+++ server-config.js
@@ -7,28 +7,9 @@ var partials = require('express-partials');
 var app = express();
 
 app.configure(function() {
-  // app.set('views', __dirname + '/views');
-  // app.set('view engine', 'ejs');
-  // app.use(partials());
-  // app.use(express.bodyParser());
-  // app.use(express.static(__dirname + '/public'));
-  // app.use(express.cookieParser('shhhh, very secret'));
-  // app.use(express.session());
+  // write app.set and app.use here
 });
 
-// app.get('/', util.checkUser, handler.renderIndex);
-// app.get('/create', util.checkUser, handler.renderIndex);
-
-// app.get('/links', util.checkUser, handler.fetchLinks);
-// app.post('/links', handler.saveLink);
-
-// app.get('/login', handler.loginUserForm);
-// app.post('/login', handler.loginUser);
-// app.get('/logout', handler.logoutUser);
-
-// app.get('/signup', handler.signupUserForm);
-// app.post('/signup', handler.signupUser);
-
-// app.get('/*', handler.navToLink);
+  // map routes to request-handler here
 
 module.exports = app;
