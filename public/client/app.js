c54522be37eb4237020dd79cd5ae45c859210b7c
ChadFong <ChadMFong@Gmail.com>
2015-03-16 21:40:18 -0700

    (add) Backbone front-end skeleton (index and router)
    Creates skeleton for front-end backbone development.  Views, models, and
    collections are not in-place, but the index pages and router are
    functional and routing

---
+++ public/client/app.js
@@ -0,0 +1,40 @@
+window.Marginal = Backbone.View.extend({
+  template: Templates.layout,
+
+  events: {
+    'click li a.index':  'renderIndexView',
+    'click li a.create': 'renderCreateView'
+  },
+
+  initialize: function(){
+    console.log( 'Marginal is running' );
+    $('body').append(this.render().el);
+
+    this.router = new Marginal.Router({ el: this.$el.find('#container') });
+    this.router.on('route', this.updateNav, this);
+
+    Backbone.history.start({ pushState: true });
+  },
+
+  render: function(){
+    this.$el.html( this.template() );
+    return this;
+  },
+
+  renderIndexView: function(e){
+    e && e.preventDefault();
+    this.router.navigate('/', { trigger: true });
+  },
+
+  renderCreateView: function(e){
+    e && e.preventDefault();
+    this.router.navigate('/create', { trigger: true });
+  },
+
+  updateNav: function(routeName){
+    this.$el.find('.navigation li a')
+      .removeClass('selected')
+      .filter('.' + routeName)
+      .addClass('selected');
+  }
+});
