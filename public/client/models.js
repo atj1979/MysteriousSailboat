26e5b42215bba17a24b3faa60ae7ab250b9ac5c0
david-hammer <dhammer126@gmail.com>
2015-03-16 22:25:25 -0700

    begin seperating models from views + slight refactor

--- public/client/models.js
+++ public/client/models.js
@@ -1,25 +1,9 @@
-var mockLoggedInUser = "Fred";
-var converter = Markdown.getSanitizingConverter();
-
 var Annotation = Backbone.Model.extend({
-  url: 'http://www.oururl.com/annotations/',
+  url: 'http://www.oururl.com/annotations/', // TODO: fix endpoints
   defaults: {
     username: '',
     text: ''
   }
-});
-var AnnotationView = Backbone.View.extend({
-  className: 'annotation',
-  template: _.template('<div class="annotation-user"><%- username %>: </div>\
-                        <div class="annotation-body"><%= text %></div>'),
-  render: function(){
-    var attrs = this.model.toJSON();
-    if (attrs.username === mockLoggedInUser) 
-      attrs.username = 'you';
-    this.$el.html(this.template(attrs));
-    return this;
-  }
-
 });
 
 var Annotations = Backbone.Collection.extend({
@@ -27,153 +11,29 @@ var Annotations = Backbone.Collection.extend({
   url: 'http://www.oururl.com/annotations/',
   show: function(){
     this.trigger('show', this);
-    console.log('here');
   },
   hide: function(){
-    this.trigger('hide', this);  }
-  });
-
-var AnnotationsView = Backbone.View.extend({
-  className: 'annotations-container',
-  initialize: function(){
-    this.collection.on('add', function(){
-      //bug - only want to render if change AND this is the focal paragraph
-      console.log('add noted')
-      this.render();
-    }, this)
-    this.collection.on('show', function(){
-      this.render();
-    }, this);
-    this.collection.on('hide', function(){
-      this.$el.html('');
-    }, this);
-  },
-  render: function(){
-    this.$el.html('');
-    this.$el.append(
-      this.collection.map(function(annotation){
-        return (new AnnotationView({model: annotation})).render().$el;
-      })
-      );
-    this.$el.append((new FormView({ collection: this.collection })).render().$el);
-    return this;
-  }
-});
-
-var FormView = Backbone.View.extend({
-  className: 'form',
-  template: _.template(' <textarea name="comment" id="comment" form="send" rows="1" cols="10"></textarea>\
-    <form action="#" id="send" method="post">\
-    <input type="submit" name="submit" class="submit"/>\
-      </form>'),
-   events: {
-    'submit #send': 'handleSubmit'
-  },
-
-  handleSubmit: function(e){
-    e.preventDefault();
-
-    var $text = this.$('#comment');
-    if ($text.val() === ''){
-      return ;
-    }
-
-    this.collection.create({
-      username: mockLoggedInUser,
-      text: converter.makeHtml( $text.val() )
-    });
-    $text.val('');
-  },
-  render: function(){
-    this.$el.html(this.template());
-    return this;
+    this.trigger('hide', this);
   }
 });
 
 var Paragraph = Backbone.Model.extend({
   initialize: function(){
     var annotations = new Annotations();
-
-    // //initialize with sample annotations
-    // annotations.add({username: 'Chad', text: 'I think this!'});
-    // annotations.add({username: 'Tony', text: 'I agree!'});
-    // annotations.add({username: 'Michael', text: 'I disagree!'});
-
     this.set('annotations', annotations);
   },
   focus: function() {
     this.trigger('focus', this);
   }
 });
-var ParagraphView = Backbone.View.extend({
-  className: 'paragraph',
-  template: _.template('<div class="count"><%= annotations.length %></div><div class="body-text"><%= text %></div>'),
-  initialize: function(){
-    var annotations = this.model.get('annotations');
-    this.annotationsView = new AnnotationsView({collection: annotations});
-
-    annotations.on('add remove', function(){
-      this.$el.find('.count').text(this.model.get('annotations').length);
-    }, this);
-
-  },
-  events: { 'click p' : 'clicked'},
-
-  clicked: function(){
-    //paragraph emits a focus event
-    this.model.focus();
-  },
-  render: function(){
-    this.$el.html(this.template(this.model.toJSON()));
-    this.$el.append(this.annotationsView.$el);
-
-    // roughly center count (easier than CSS!)
-    $ (function(){ 
-      this.$el.find('.count').animate( { top: this.$el.find('.body-text').height() / 2 - 20, left: -22 }, 0 );
-    }.bind(this))
-
-    return this;
-  }
-
-});
 
 var Paragraphs = Backbone.Collection.extend({
   model: Paragraph
 });
-var ParagraphsView = Backbone.View.extend({
-  tagName: 'div',
-  className: 'pars-container',
-  initialize: function(){
-    this.focalPar = null;
-    this.collection.on('focus', function(paragraph){
-
-
-      if (this.focalPar !== paragraph){
-        if (this.focalPar) {
-          this.focalPar.get('annotations').hide();
-        }
-        this.focalPar = paragraph;
-        this.focalPar.get('annotations').show();
-      }
-    });
-
-  },
-  render: function(){
-    this.$el.append(
-      this.collection.map(function(paragraph){
-        var x = new ParagraphView({model: paragraph});
-        x.render();
-        return x.$el;
-      })
-      );
-    return this;
-  }
-});
 
 var Document = Backbone.Model.extend({
   initialize: function(resp){
-
-    var regex = /<p>(.|\n)*?<\/p>/g; 
+    var regex = /<p>(.|\n)*?<\/p>/g; //currently splits on paragraph. TODO: include other assets, like images
     var pars = resp.content.match(regex)
     .map(function(par){
       return new Paragraph({ text : par }); 
@@ -183,23 +43,7 @@ var Document = Backbone.Model.extend({
   }
 });
 
-var DocumentView = Backbone.View.extend({
-  tagName: 'div',
-  template: _.template("<h2><%= title %></h2>"),
-  initialize: function(){
-    this.paragraphsView = new ParagraphsView({collection: this.model.get('paragraphs')});
-    this.render();
-  },
-  render: function(){
-    this.$el.html(this.template(this.model.toJSON()));
-    this.$el.append(this.paragraphsView.$el);
-    this.paragraphsView.render();
-    return this;
-  }
-
-});
-
-
+//Hardcoded data -- for now.  this is a sample response from the readability parse API
 var data = {
   "domain": "www.newyorker.com",
   "next_page_id": null,
