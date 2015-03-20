8ea8614b2e976e0376576a283f1fb9cc4ad341e0
Michael Roberts <m_roberts6@u.pacific.edu>
2015-03-20 00:14:58 -0700

    (feat) rudimentary implementation of group model and view (backbone)

--- public/client/views/groupView.js
+++ public/client/views/groupView.js
@@ -1,1 +1,13 @@
 // backbone view for a list of usernames in a group
+
+Marginalio.GroupView = Backbone.View.extend({
+  className: 'group',
+  template: _.template('<div class="group-user"><%- username %>: </div>'),
+  render: function(){
+    var attrs = this.model.toJSON();
+    if (attrs.username === mockLoggedInUser) 
+      attrs.username = 'you';
+    this.$el.html(this.template(attrs));
+    return this;
+  }
+});
